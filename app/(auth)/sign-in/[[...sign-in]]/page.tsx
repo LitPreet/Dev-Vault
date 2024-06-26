2fd9a1937602653cbcf9c6e0b31028daf1f737e7
preet006 <preetbhardwaj146@gmail.com>
2024-06-26 11:01:20 +0530

    feat: add layouts

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className="flex-center h-screen w-full">
+    <div>
       <SignIn />
     </div>
   )
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className="flex-center glassmorphism-auth h-screen w-full">
+    <div>
       <SignUp />
     </div>
   )
