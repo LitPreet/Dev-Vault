e21f647fbc64055e444f29cf7f97f7023c17b91c
preet006 <preetbhardwaj146@gmail.com>
2024-06-29 18:26:09 +0530

    feat: tags and their detail page

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -2,7 +2,10 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetTopInteractedTagsParams,GetQuestionsByTagIdParams,GetAllTagsParams} from "./shared.types";
+import { FilterQuery } from "mongoose";
+import Tag, { ITag } from "@/database/tag.model";
+import Question from "@/database/question.model";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
     try {
@@ -15,3 +18,123 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
         console.log(err)
     }
 }
+
+export async function getAllTags(params: GetAllTagsParams) {
+    try {
+      connectToDatabase();
+  
+      const { searchQuery, filter, page = 1, pageSize = 20 } = params;
+  
+      const skipAmount = (page - 1) * pageSize;
+  
+      const query: FilterQuery<typeof Tag> = {};
+  
+      if (searchQuery) {
+        query.$or = [
+          {
+            name: { $regex: new RegExp(searchQuery, "i") },
+          },
+        ];
+      }
+  
+      let sortOptions = {};
+  
+      switch (filter) {
+        case "popular":
+          sortOptions = { questions: -1 };
+          break;
+        case "recent":
+          sortOptions = { createdOn: -1 };
+          break;
+        case "name":
+          sortOptions = { name: 1 };
+          break;
+        case "old":
+          sortOptions = { createdOn: 1 };
+          break;
+        default:
+          break;
+      }
+  
+      const totalTags = await Tag.countDocuments();
+  
+      const tags = await Tag.find(query)
+        .sort(sortOptions)
+        .skip(skipAmount)
+        .limit(pageSize);
+  
+      const isNext = totalTags > skipAmount + tags.length;
+  
+      return { tags, isNext };
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+export async function getQuestionsByTagId(params: GetQuestionsByTagIdParams) {
+    try {
+      connectToDatabase();
+  
+      const { tagId, searchQuery, page = 1, pageSize = 20 } = params;
+  
+      const skipAmount = (page - 1) * pageSize;
+  
+      const tagFilter: FilterQuery<ITag> = { _id: tagId };
+  
+      const tag = await Tag.findOne(tagFilter).populate({
+        path: "questions",
+        model: Question,
+        match: searchQuery
+          ? { title: { $regex: searchQuery, $options: "i" } }
+          : {},
+        options: {
+          sort: { createdAt: -1 },
+          skip: skipAmount,
+          limit: pageSize + 1,
+        },
+        populate: [
+          {
+            path: "tags",
+            model: Tag,
+            select: "_id name",
+          },
+          {
+            path: "author",
+            model: User,
+            select: "_id clerkId name picture",
+          },
+        ],
+      });
+  
+      if (!tag) {
+        throw new Error("Tag not found");
+      }
+  
+      const questions = tag.questions;
+  
+      const isNext = questions.length > pageSize;
+  
+      return { tagTitle: tag.name, questions, isNext };
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+  
+  export async function getTopTags() {
+    try {
+      connectToDatabase();
+  
+      const topTags = await Tag.aggregate([
+        { $project: { name: 1, numberOfQuestions: { $size: "$questions" } } },
+        { $sort: { numberOfQuestions: -1 } },
+        { $limit: 5 },
+      ]);
+  
+      return topTags;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
