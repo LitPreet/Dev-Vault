c51ae4ba1692a0f2b1376c89b51d071edc692bd0
preet006 <preetbhardwaj146@gmail.com>
2024-07-01 12:20:03 +0530

    feat: pagination

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,11 +4,14 @@ import LocalSearch from "@/components/shared/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import { SearchParamsProps } from "@/types/index";
-// import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import type { Metadata } from "next";
 import Link from "next/link";
 import NoResult from "@/components/shared/NoResult";
-import { getQuestions } from "@/lib/actions/question.action";
+import {
+  getQuestions,
+  getRecommendedQuestions,
+} from "@/lib/actions/question.action";
 import QuestionCard from "@/components/cards/QuestionCard";
 import Pagination from "@/components/shared/Pagination";
 
@@ -17,35 +20,35 @@ export const metadata: Metadata = {
 };
 
 export default async function Home({ searchParams }: SearchParamsProps) {
-  // const { userId } = auth();
+  const { userId } = auth();
 
   let result;
   result = await getQuestions({
+    searchQuery: searchParams.q,
+    filter: searchParams.filter,
+    page: searchParams.page ? +searchParams.page : 1,
+  });
+
+  if (searchParams?.filter === "recommended") {
+    if (userId) {
+      result = await getRecommendedQuestions({
+        userId,
         searchQuery: searchParams.q,
-        filter: searchParams.filter,
         page: searchParams.page ? +searchParams.page : 1,
       });
-
-  // if (searchParams?.filter === "recommended") {
-  //   if (userId) {
-  //     result = await getRecommendedQuestions({
-  //       userId,
-  //       searchQuery: searchParams.q,
-  //       page: searchParams.page ? +searchParams.page : 1,
-  //     });
-  //   } else {
-  //     result = {
-  //       questions: [],
-  //       isNext: false,
-  //     };
-  //   }
-  // } else {
-  //   result = await getQuestions({
-  //     searchQuery: searchParams.q,
-  //     filter: searchParams.filter,
-  //     page: searchParams.page ? +searchParams.page : 1,
-  //   });
-  // }
+    } else {
+      result = {
+        questions: [],
+        isNext: false,
+      };
+    }
+  } else {
+    result = await getQuestions({
+      searchQuery: searchParams.q,
+      filter: searchParams.filter,
+      page: searchParams.page ? +searchParams.page : 1,
+    });
+  }
 
   return (
     <>
@@ -58,7 +61,6 @@ export default async function Home({ searchParams }: SearchParamsProps) {
           </Button>
         </Link>
       </div>
-
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearch
           route="/"
@@ -73,9 +75,7 @@ export default async function Home({ searchParams }: SearchParamsProps) {
           containerClasses="hidden max-md:flex"
         />
       </div>
-
       <HomeFilters />
-
       <div className="mt-10 flex w-full flex-col gap-6">
         {result.questions.length > 0 ? (
           result.questions.map((question) => (
@@ -104,8 +104,8 @@ export default async function Home({ searchParams }: SearchParamsProps) {
         <Pagination
           pageNumber={searchParams?.page ? +searchParams.page : 1}
           isNext={result.isNext}
-        />
-      </div>
+          />
+          </div>
     </>
   );
 }
--- components/shared/Pagination.tsx
+++ components/shared/Pagination.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { formUrlQuery } from "@/lib/utils";
 import { Button } from "../ui/button";
 import { useRouter, useSearchParams } from "next/navigation";
@@ -28,26 +27,26 @@ const Pagination = ({ pageNumber, isNext }: Props) => {
   if (!isNext && pageNumber === 1) return null;
 
   return (
-    <div className="flex w-full items-center justify-center gap-2">
-      <Button
-        disabled={pageNumber === 1}
-        onClick={() => handleNavigation("prev")}
-        className="light-border-2 btn flex min-h-[36px] items-center justify-center gap-2 border"
-      >
-        <p className="body-medium text-dark200_light800">Prev</p>
-      </Button>
-      <div className="flex items-center justify-center rounded-md bg-primary-500 px-3.5 py-2">
-        <p className="body-semibold text-light-900">{pageNumber}</p>
-      </div>
-      <Button
-        disabled={!isNext}
-        onClick={() => handleNavigation("next")}
-        className="light-border-2 btn flex min-h-[36px] items-center justify-center gap-2 border"
-      >
-        <p className="body-medium text-dark200_light800">Next</p>
-      </Button>
-    </div>
-  );
+  <div className="flex w-full items-center justify-center gap-2">
+  <Button
+    disabled={pageNumber === 1}
+    onClick={() => handleNavigation("prev")}
+    className="light-border-2 btn flex min-h-[36px] items-center justify-center gap-2 border"
+  >
+    <p className="body-medium text-dark200_light800">Prev</p>
+  </Button>
+  <div className="flex items-center justify-center rounded-md bg-primary-500 px-3.5 py-2">
+    <p className="body-semibold text-light-900">{pageNumber}</p>
+  </div>
+  <Button
+    disabled={!isNext}
+    onClick={() => handleNavigation("next")}
+    className="light-border-2 btn flex min-h-[36px] items-center justify-center gap-2 border"
+  >
+    <p className="body-medium text-dark200_light800">Next</p>
+  </Button>
+</div>
+);
 };
 
 export default Pagination;
